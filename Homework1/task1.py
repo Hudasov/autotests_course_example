a3fe15286f1e521f436237225a29b8d244612e62
vm.hudasov <vm.hudasov@tensor.ru>
2024-05-10 09:27:21 +0300

    Homeworks 4-5

--- Homework1/task1.py
+++
@@ -1,9 +0,0 @@
-def hello_world():
-    """Привет, мир!"""
-
-    # todo Здесь нужно написать код
-
-    hello = "Hello "
-    world = "world"
-    hello_and_world = hello + world + "!"
-    return hello_and_world
--- Homework2/task6.py
+++
@@ -1,8 +0,0 @@
-def delete_symbols(string):
-    """Удаление символов с нечетными индексами
-    :param string: строка
-    :return: строку без символов с нечетными индексами исходной строки
-    """
-    # todo Здесь нужно написать код
-    new_string = string[::2]
-    return new_string
